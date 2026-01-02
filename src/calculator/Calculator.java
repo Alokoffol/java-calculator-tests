13f4d2508c9b9cc59215d6b0ce440796c981ac30
Oleg Alymov <alymoffol@yandex.ru>
2026-01-02 12:18:06 +0300

    feat: implement basic calculator operations
    - Add 6 mathematical methods
    - Add exception handling
    - Add manual testing in Main

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,4 +1,34 @@
 package calculator;
 
 public class Calculator {
+
+    public int sumNumber(int a, int b) {
+        return a + b;
+    }
+
+    public int subNumber(int a, int b) {
+        return a - b;
+    }
+
+    public int multiNumber(int a, int b) {
+        return a * b;
+    }
+
+    public double divNumber(int a, int b) {
+        if(b == 0) {
+            throw new ArithmeticException("На ноль делить нельзя");
+        }
+        return (double) a / b;
+    }
+
+    public double powNumber(int a, int b) {
+        return Math.pow(a, b);
+    }
+
+    public double sqrtNumber(int a) {
+        if (a < 0) {
+            throw new IllegalArgumentException("Корень из отрицательного числа невозможен");
+        }
+        return Math.sqrt(a);
+    }
 }
--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -1,9 +1,29 @@
 package calculator;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+
+        System.out.println("5 + 3 = " + calc.sumNumber(5, 3));
+        System.out.println("7 - 4 = " + calc.subNumber(7, 4));
+        System.out.println("2 * 8 = " + calc.multiNumber(2, 8));
+        System.out.println("9 / 2 = " + calc.divNumber(9, 2));
+
+        try{
+            System.out.println("4 / 0 = " + calc.divNumber(4, 0));
+        } catch(ArithmeticException e) {
+            System.out.println("5 / 0 = Ошибка: " + e.getMessage());
+        }
+
+        System.out.println("2^3 = " + calc.powNumber(2, 3));
+        System.out.println("√25 = " + calc.sqrtNumber(25));
+
+        try {
+            System.out.println("√-4 = " + calc.sqrtNumber(-4));
+        } catch (IllegalArgumentException e) {
+            System.out.println("√-4 = Ошибка: " + e.getMessage());
+        }
 
     }
 }
